a3d3ed269094093ff6f74eb3097a20f1fe733a55
shushinde111@gmail.com <shushinde111@gmail.com>
2024-06-14 14:00:53 +0530

    data added as json

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import env from "dotenv";
 import express from "express";
 import leaveData from "./leaveData.json" assert { type: "json" };
 
+const data = leaveData;
+
 env.config();
 
 const app = express();
 
 app.get("/api/v1", (req, res) => {
   console.log("new req");
-  res.send(leaveData);
+  res.json(data);
 });
 
 app.listen(process.env.PORT, () => {
